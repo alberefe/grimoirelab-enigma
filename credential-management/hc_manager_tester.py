205a0e29e3ab58f549cdc3a3da4d219d61e1b708
alberefe <alberefe@tutanota.com>
2024-11-12 19:31:53 +0100

    Added hc_manager_tester.py

---
+++ credential-management/hc_manager_tester.py
@@ -0,0 +1,29 @@
+import os
+import json
+from dotenv import load_dotenv
+from hc_manager import HashicorpManager
+
+
+# Load the vault configuration
+with open("/home/noname/Documents/University/PFG/grimoirelab-dev/sources/grimoirelab-credential-management/config_files/hashicorp_vault_config.json", "r") as file:
+    vault_config = json.load(file)
+
+# Get the vault url
+vault_url = vault_config["vault_url"]
+print(vault_url)
+
+# Load the environment variables
+load_dotenv()
+
+# Assign useful variables
+certificate = os.getenv("VAULT_CACERT")
+token = os.getenv("VAULT_TOKEN")
+vault_unseal_key = vault_config["vault_unseal_key"]
+
+# Instantiate the class
+hashicorp_manager = HashicorpManager(vault_url, token, certificate)
+
+# Get the secret (it assigns it to env var)
+hashicorp_manager.get_secret("bugzilla")
+print("Variable de entorno asignada: " + os.getenv("BUGZILLA_USERNAME"))
+

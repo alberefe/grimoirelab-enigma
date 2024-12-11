3d47b7cf9b38bee36f7586aedfa2bc1065946a55
alberefe <alberefe@tutanota.com>
2024-12-11 10:54:46 +0100

    Deleted files used to test the package.

--- enigma/aws_manager_play.py
+++
@@ -1,8 +0,0 @@
-from aws_manager import AwsManager
-
-
-# Creates the object with the credentials in the .aws/credentials file
-aws_manager = AwsManager()
-
-print(aws_manager.get_secret("bugzilla", "username"))
-print(aws_manager.get_secret("bugzilla", "password"))

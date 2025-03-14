b03acb0ac383f2fccd66837f256ffa2fb57716de
Alberto Ferrer Sánchez <alberefe@gmail.com>
2025-03-14 19:20:21 +0100

    Changed from singleton to factory of managers.

--- enigma/secrets_manager_factory.py
+++ enigma/secrets_manager_factory.py
@@ -17,13 +17,12 @@
 #     Alberto Ferrer Sánchez (alberefe@gmail.com)
 #
 
-import os
 import getpass
 import logging
-from typing import Optional
+import os
 
-from .bw_manager import BitwardenManager
 from .aws_manager import AwsManager
+from .bw_manager import BitwardenManager
 from .hc_manager import HashicorpManager
 
 logging.basicConfig(
@@ -33,12 +32,9 @@ _logger = logging.getLogger(__name__)
 
 
 class SecretsManagerFactory:
-    _bw_manager: Optional[BitwardenManager] = None
-    _aws_manager: Optional[AwsManager] = None
-    _hc_manager: Optional[HashicorpManager] = None
 
-    @classmethod
-    def get_bitwarden_manager(cls, email=None, password=None):
+    @staticmethod
+    def get_bitwarden_manager(email=None, password=None):
         """
         Gets or creates a BitwardenManager instance.
 
@@ -54,42 +50,38 @@ class SecretsManagerFactory:
         Raises:
             ValueError: If credentials cannot be obtained
         """
-        if cls._bw_manager is None:
-            _logger.debug("Creating new Bitwarden manager")
+        _logger.debug("Creating new Bitwarden manager")
 
-            if email is None:
-                email = os.environ.get("BW_EMAIL")
-            if password is None:
-                password = os.environ.get("BW_PASSWORD")
+        if email is None:
+            email = os.environ.get("BW_EMAIL")
+        if password is None:
+            password = os.environ.get("BW_PASSWORD")
+
+        if not email or not password:
+            email = input("Bitwarden email: ")
+            password = getpass.getpass("Bitwarden master password: ")
 
             if not email or not password:
-                email = input("Bitwarden email: ")
-                password = getpass.getpass("Bitwarden master password: ")
+                raise ValueError("Bitwarden credentials are required")
 
-                if not email or not password:
-                    raise ValueError("Bitwarden credentials are required")
+        return BitwardenManager(email, password)
 
-            cls._bw_manager = BitwardenManager(email, password)
-
-        return cls._bw_manager
-
-    @classmethod
-    def get_aws_manager(cls):
+    @staticmethod
+    def get_aws_manager():
         """
         Gets or creates an AwsManager instance.
 
         Returns:
             AwsManager: The singleton AwsManager instance
         """
-        if cls._aws_manager is None:
-            _logger.debug("Creating new AWS manager")
-            cls._aws_manager = AwsManager()
+        _logger.debug("Creating new AWS manager")
 
-        return cls._aws_manager
+        return AwsManager()
 
-    @classmethod
+
+    @staticmethod
     def get_hashicorp_manager(
-        cls, vault_addr=None, token=None, certificate=None
+        vault_addr=None, token=None, certificate=None
     ):
         """
         Gets or creates a HashicorpManager instance.
@@ -107,39 +99,25 @@ class SecretsManagerFactory:
         Raises:
             ValueError: If required credentials cannot be obtained
         """
-        if cls._hc_manager is None:
-            _logger.debug("Creating new Hashicorp manager")
+        _logger.debug("Creating new Hashicorp manager")
 
-            if vault_addr is None:
-                vault_addr = os.environ.get("VAULT_ADDR")
-            if token is None:
-                token = os.environ.get("VAULT_TOKEN")
-            if certificate is None:
-                certificate = os.environ.get("VAULT_CACERT")
+        if vault_addr is None:
+            vault_addr = os.environ.get("VAULT_ADDR")
+        if token is None:
+            token = os.environ.get("VAULT_TOKEN")
+        if certificate is None:
+            certificate = os.environ.get("VAULT_CACERT")
 
-            if not vault_addr:
-                vault_addr = input("Please enter vault address: ")
-            if not token:
-                token = input("Please enter vault token: ")
-            if not certificate:
-                certificate = input(
-                    "Please enter path to a PEM-encoded CA certificate file: "
-                )
+        if not vault_addr:
+            vault_addr = input("Please enter vault address: ")
+        if not token:
+            token = input("Please enter vault token: ")
+        if not certificate:
+            certificate = input(
+                "Please enter path to a PEM-encoded CA certificate file: "
+            )
 
-            if not all([vault_addr, token, certificate]):
-                raise ValueError("All Hashicorp Vault credentials are required")
+        if not all([vault_addr, token, certificate]):
+            raise ValueError("All Hashicorp Vault credentials are required")
 
-            cls._hc_manager = HashicorpManager(vault_addr, token, certificate)
-
-        return cls._hc_manager
-
-    @classmethod
-    def reset_managers(cls):
-        """
-        Resets all manager instances.
-
-        For testing purposes
-        """
-        cls._bw_manager = None
-        cls._aws_manager = None
-        cls._hc_manager = None
+        return HashicorpManager(vault_addr, token, certificate)
